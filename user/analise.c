521b8b5f8edb2059fd2a51102e168d61869e4401
VictorMS-200 <victorm20005@gmail.com>
2026-07-20 11:31:41 -0300

    Função analise para testar o escalonador

---
+++ user/analise.c
@@ -0,0 +1,42 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+void trabalho() {
+    int pid = getpid();
+
+    // Inicia o contador de ticks
+    int start_time = uptime();
+
+    // Realiza uma tarefa pesada
+    long soma = 0;
+    long maximo = 5000000000; 
+    for (long i = 0; i < maximo; i++) {
+        soma = soma + 1;
+    }
+
+    // Termina o contador de ticks
+    int end_time = uptime();
+
+    int total_time = end_time - start_time;
+    
+    printf("O processo %d terminou em %d ticks.\n", pid, total_time);
+    exit(0);
+}
+
+int main(void) {
+    int numero_processos = 10;
+
+    // Cria os processos filhos
+    for (int i = 0; i < numero_processos; i++) {
+        int pid = fork();
+        if (pid == 0) {
+            trabalho();
+        }
+    }
+    // Espera todos os processos filhos terminarem
+    for (int i = 0; i < numero_processos; i++) {
+        wait(0);
+    }
+    return 0;
+}
